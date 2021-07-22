f8ba110cd8c21598c6656f5d24ab27f6eda9c068
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-07-21 23:29:25 -0300

    Add LogCard component

---
+++ src/components/LogCard/LogCard.js
@@ -0,0 +1,14 @@
+import { Container, RowCell } from './styles';
+
+const LogCard = () => {
+  return (
+    <Container>
+      <RowCell>1 Jul, Thu</RowCell>
+      <RowCell>8:00 - 18:00</RowCell>
+      <RowCell>24hrs</RowCell>
+      <RowCell>Sample comment</RowCell>
+    </Container>
+  );
+};
+
+export default LogCard;
---
+++ src/components/LogCard/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.tr`
+  &:hover {
+    background-color: rgba(135, 158, 207, 0.1);
+  }
+`;
+
+export const RowCell = styled.td`
+  padding: 8px;
+  font-size: 14px;
+  border-bottom: 1px solid #e3e3e3;
+`;
