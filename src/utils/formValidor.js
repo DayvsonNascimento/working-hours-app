abac949cf9457c0eab5653ce62408234d368642b
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-07-25 21:11:36 -0300

    Add form validator util

---
+++ src/utils/formValidor.js
@@ -0,0 +1,29 @@
+export const validate = (values) => {
+  const errors = {};
+
+  if (!values.fullName) {
+    errors.fullName = 'Required';
+  } else if (values.fullName.length > 50) {
+    errors.fullName = 'Must be 50 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length < 6) {
+    errors.password = 'Must be at least 6 characters';
+  }
+
+  if (!values.confirmPassword) {
+    errors.confirmPassword = 'Required';
+  } else if (values.confirmPassword !== values.password) {
+    errors.confirmPassword = 'Passwords must match';
+  }
+
+  return errors;
+};
