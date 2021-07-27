c84e5305d1ff8d9ebe528ec511657a065311574b
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-07-27 20:38:46 -0300

    Add notifications in user api requests

--- src/services/user/User.js
+++ src/services/user/User.js
@@ -1,26 +1,22 @@
 import api from '../api';
 import * as notificationsUtil from '../../utils/notificationsUtil';
+import { removeToken } from '../../utils/authCookieUtil';
 
 export const saveWorkDaySchedule = async (body) => {
   try {
     const data = await api.post('/workSchedule', body);
 
     notificationsUtil.createSuccessNotification({
-      message: 'Data saved successfully!'
-    })
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const loadWorkSchedule = async (params) => {
-  try {
-    const { data } = await api.get('/workSchedule', { params: params });
+      message: 'Data saved successfully!',
+    });
 
     return data;
   } catch (error) {
-    console.log(error);
+    const message =
+      error?.response?.data?.error || 'Could not save data, please try again.';
+    notificationsUtil.createDangerNotification({
+      message: message,
+    });
   }
 };
 
@@ -30,6 +26,24 @@ export const getBasicInfo = async () => {
 
     return data;
   } catch (error) {
-    console.log(error);
+    const statusCode = error?.response?.status;
+
+    if (statusCode === 403) {
+      removeToken();
+    }
+  }
+};
+
+export const loadWorkSchedule = async (params) => {
+  try {
+    const { data } = await api.get('/workSchedule', { params: params });
+
+    return data;
+  } catch (error) {
+    const message =
+      error?.response?.data?.error || 'Could not load data, please reload the page.';
+    notificationsUtil.createDangerNotification({
+      message: message,
+    });
   }
 };
