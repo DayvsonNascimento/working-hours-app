9c2dba4ac8975f440aa49bb6fc527540c556fdbe
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2021-07-28 10:05:59 -0300

    Add LogTable tests

---
+++ src/components/LogTable/__test__/logTable.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import * as API from '../../../services/user/User';
+import LogTable from '../LogTable';
+
+describe('LogTable component should', () => {
+  it('render without errors', () => {
+    render(<LogTable />);
+  });
+
+  it('display default text labels', () => {
+    render(<LogTable />);
+
+    expect(screen.getByText('Date')).toBeInTheDocument();
+    expect(screen.getByText('Hours range')).toBeInTheDocument();
+    expect(screen.getByText('Total')).toBeInTheDocument();
+  });
+
+  it('display selectd month and year', () => {
+    const month = 7;
+    const monthName = 'July';
+    const year = '2021';
+    render(<LogTable month={month} />);
+
+    expect(screen.getByDisplayValue(monthName)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(year)).toBeInTheDocument();
+  });
+});
